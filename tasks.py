685b51f7b29794515a549f54f4eefdf643774343
MichaelWu0012@gmail.com <MichaelWu0012@gmail.com>
2020-04-10 04:44:36 +1000

    added wnli task processor

--- tasks.py
+++ tasks.py
@@ -382,6 +382,48 @@ class QqpProcessor(DataProcessor):
         return examples
 
 
+class WnliProcessor(DataProcessor):
+    """Processor for the MultiNLI data set (GLUE version)."""
+    TASK_TYPE = TaskType.CLASSIFICATION
+
+    def get_train_examples(self, data_dir):
+        """See base class."""
+        return self._create_examples(
+            self._read_tsv(os.path.join(data_dir, "train.tsv")), "train")
+
+    def get_dev_examples(self, data_dir):
+        """See base class."""
+        return self._create_examples(
+            self._read_tsv(os.path.join(data_dir, "dev.tsv")),
+            "dev")
+
+    def get_test_examples(self, data_dir):
+        """See base class."""
+        return self._create_examples(
+            self._read_tsv(os.path.join(data_dir, "test.tsv")), "test")
+
+    def get_labels(self):
+        """See base class."""
+        return ["0", "1"]
+
+    def _create_examples(self, lines, set_type):
+        """Creates examples for the training and dev sets."""
+        examples = []
+        for (i, line) in enumerate(lines):
+            if i == 0:
+                continue
+            guid = "%s-%s" % (set_type, line[0])
+            text_a = line[1]
+            text_b = line[2]
+            if set_type == "test":
+                label = "0"
+            else:
+                label = line[-1]
+            examples.append(
+                InputExample(guid=guid, text_a=text_a, text_b=text_b, label=label))
+        return examples
+
+
 PROCESSORS = {
     "mrpc": MrpcProcessor,
     "qqp": QqpProcessor,
